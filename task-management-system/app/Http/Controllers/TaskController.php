876ef2ff5014400413b5be565d256a35a3b58d87
shenalwijayasinghe <2001.wijayasinghe@gmail.com>
2024-10-21 16:40:20 +0530

    Merge branch 'main' of https://github.com/shenalwijayasinghe/Task_Management_System-Group10

--- task-management-system/app/Http/Controllers/TaskController.php
+++ task-management-system/app/Http/Controllers/TaskController.php
@@ -14,40 +14,5 @@ class TaskController extends Controller
         return view('usertask.index',compact('usertasks'));
     }
 
-    public function create(){
-        return view('usertask.create');
-    }
-    public function store(StoreTaskRequest $request) 
-    { 
-        Usertask::create($request->validated());
-        return redirect('usertask')->with('success', 'Task created successfully'); 
-    } 
- 
-    public function all() 
-    { 
-        return Usertask::all(); 
-    } 
- 
-    public function get($id) 
-    { 
-        return Usertask::findOrFail($id); 
-    } 
- 
-    public function edit($id) 
-    { 
-        return Usertask::findOrFail($id); 
-    } 
- 
-    public function update(Request $request, $id) 
-    { 
-        $usertask = Usertask::findOrFail($id); 
-        return $usertask->update($request->all()); 
-    } 
- 
-    public function delete($id) 
-    { 
-        $usertask = Usertask::findOrFail($id); 
-        return $usertask->delete(); 
-    } 
-    
+  
 }
