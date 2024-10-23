5088e9fc4e274f8d5ae383a1dfadd29d37848db2
TDRajapaksha <tdrajapaksha1212@gmail.com>
2024-10-23 15:54:41 +0530

    SEU_IS_20_ICT_012: edit

--- task-management-system/app/Http/Controllers/TaskController.php
+++ task-management-system/app/Http/Controllers/TaskController.php
@@ -8,21 +8,7 @@ use App\Models\Usertask;
 
 class TaskController extends Controller
 {
-    
-    public function index(){
-        $usertasks = Usertask::all();
-        return view('usertask.index', compact('usertasks'));
-    }
 
-    public function create(){
-        return view('usertask.create');
-    }
-
-    public function store(StoreTaskRequest $request) 
-    { 
-        Usertask::create($request->validated());
-        return redirect('usertask')->with('success', 'Task created successfully'); 
-    }
 
     public function all() 
     { 
@@ -37,21 +23,9 @@ class TaskController extends Controller
     public function edit($id) 
     { 
         $usertask = Usertask::findOrFail($id); 
-        return view('usertask.edit', compact('usertask')); // Corrected to pass the single task
+        return view('usertask.edit', compact('usertask')); 
     }
 
-    public function update(Request $request, $id) 
-    { 
-        $usertask = Usertask::findOrFail($id); 
-        $usertask->update($request->all()); 
-        return redirect('usertask')->with('success', 'Task updated successfully.'); 
-    }
-
-    public function delete($id) 
-    { 
-        $usertask = Usertask::findOrFail($id); 
-        $usertask->delete(); 
-        return redirect('usertask')->with('success', 'Task Delete successfully.');
-    }
+    
 }
 
