23888295a7e757705a809cbd9c49c523967c78a4
TDRajapaksha <tdrajapaksha1212@gmail.com>
2024-10-23 23:17:38 +0530

    SEU_IS_20_ICT_012: edit

--- task-management-system/app/Http/Controllers/TaskController.php
+++ task-management-system/app/Http/Controllers/TaskController.php
@@ -9,19 +9,18 @@ use App\Models\Usertask;
 class TaskController extends Controller
 {
 
-
-    public function all() 
-    { 
-        return Usertask::all(); 
+    public function all()
+    {
+        return Usertask::all();
     }
 
-    public function get($id) 
-    { 
-        return Usertask::findOrFail($id); 
+    public function get($id)
+    {
+        return Usertask::findorFail($id);
     }
 
-    public function edit($id) 
-    { 
+    public function edit($id)
+    {
         $usertask = Usertask::findOrFail($id); 
         return view('usertask.edit', compact('usertask')); 
     }
