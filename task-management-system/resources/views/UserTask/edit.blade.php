be009d02f59413dcf74bd56135c5e79a3a8ce763
TDRajapaksha <tdrajapaksha1212@gmail.com>
2024-11-02 19:11:42 +0530

    SEU/IS/20/ICT/012 edit.blead.php

---
+++ task-management-system/resources/views/UserTask/edit.blade.php
@@ -0,0 +1,35 @@
+@extends('UserTask.layout')
+@section('content')
+
+<div class="card mt-5">
+  <h2 class="card-header text-primary">Edit Task</h2>
+  <div class="card-body">
+  <form action="{{url('UserTask/update/' . $UserTask->id) }}" method="POST">
+
+  @csrf
+
+
+  <div>
+      <label for="task" class="form-lablel">Task</label>
+      <input type="text" name="task" class="form-control" id="task" value="{{ $UserTask->task}}" required>
+    </div>
+
+    <div>
+      <label for="description" class="form-lablel">Description</label>
+      <input type="text" name="description" class="form-control" id="description" value="{{ $UserTask->description}}" required>
+    </div>
+
+    <div>
+      <label for="duedate" class="form-lablel">DueDate</label>
+      <input type="date" name="duedate" class="form-control" id="duedate" value="{{ $UserTask->duedate}}" required>
+    </div>
+
+    <div>
+      <label for="status" class="form-lablel">Status</label>
+      <input type="text" name="status" class="form-control" id="status" value="{{ $UserTask->status}}" required>
+    </div>
+
+    <button type="submit" class="mt-2 btn btn-primary">Update Task</button>
+  </form>
+  </div>
+</div>
